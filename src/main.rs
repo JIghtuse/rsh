77bc5549f04ff2e98df46fafa5bfd70b09e4f9f7
Boris Egorov <egorov@linux.com>
2015-10-08 16:25:37 +0600

    First version able to run commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::io::prelude::*;
+use std::process::Command;
+
+fn execute_command(line: String) {
+    let mut words = line.split_whitespace();
+
+    let mut command = Command::new(words.next().unwrap());
+    for word in words {
+        command.arg(word);
+    }
+
+    command.spawn();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        execute_command(line.unwrap());
+    }
 }
