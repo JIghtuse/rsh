de804fb3743dc3ffd7c3345f280be2257523a4ac
Boris Egorov <egorov@linux.com>
2015-10-13 23:58:14 +0600

    Add special command "cd" and process it

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::io;
 use std::io::prelude::*;
 use std::process::Command;
+use std::env;
 
 fn execute_command(line: String) {
     let mut words = line.split_whitespace();
 
-    let mut command = Command::new(words.next().unwrap());
-    for word in words {
-        command.arg(word);
-    }
+    if let Some(program) = words.next() {
+        if program == "cd" {
+            if let Some(path) = words.next() {
+                env::set_current_dir(path).unwrap();
+            }
+            return;
+        }
 
-    command.spawn();
+        let mut command = Command::new(program);
+        for word in words {
+            command.arg(word);
+        }
+
+        command.spawn();
+    }
 }
 
 fn main() {
