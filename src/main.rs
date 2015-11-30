e507c060e99a5e7e85c743b15352ca320fdf28d5
Boris Egorov <egorov@linux.com>
2015-12-01 00:48:06 +0600

    Add `pwd` built-in; reformat built-in calls

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,32 @@ use std::io;
 use std::io::prelude::*;
 use std::process::Command;
 use std::env;
+use std::str::SplitWhitespace;
+use std::collections::HashMap;
 
-fn execute_command(line: String) {
+type BuiltinCommand = fn(&mut SplitWhitespace);
+
+fn cd(paths: &mut SplitWhitespace) {
+    if let Some(path) = paths.next() {
+        if let Err(error) = env::set_current_dir(path) {
+            println!("Cannot change directory: {}", error)
+        }
+    }
+}
+
+fn pwd(_: &mut SplitWhitespace) {
+    match env::current_dir() {
+        Ok(current_dir) => println!("{}", current_dir.display()),
+        Err(error) => println!("Cannot print current dir: {}", error),
+    }
+}
+
+fn execute_command(line: String, builtins: &HashMap<&str, BuiltinCommand>) {
     let mut words = line.split_whitespace();
 
     if let Some(program) = words.next() {
-        if program == "cd" {
-            if let Some(path) = words.next() {
-                env::set_current_dir(path).unwrap();
-            }
+        if builtins.contains_key(program) {
+            builtins[program](&mut words);
             return;
         }
 
@@ -24,9 +41,13 @@ fn execute_command(line: String) {
 }
 
 fn main() {
+    let mut builtins: HashMap<&str, BuiltinCommand> = HashMap::new();
+    builtins.insert("cd", cd);
+    builtins.insert("pwd", pwd);
+
     let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
-        execute_command(line.unwrap());
+        execute_command(line.unwrap(), &builtins);
     }
 }
