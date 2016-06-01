6b8fc95d1c8cd8cdbc351cd46036834755fa45dc
Boris Egorov <egorov@linux.com>
2016-06-01 16:55:08 +0600

    main: Add our own error type, propagate errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::convert::From;
 use std::io;
 use std::io::prelude::*;
 use std::process::Command;
@@ -5,39 +6,47 @@ use std::env;
 use std::str::SplitWhitespace;
 use std::collections::HashMap;
 
-type BuiltinCommand = fn(&mut SplitWhitespace);
+#[derive(Debug)]
+enum RshError {
+    Io(io::Error)
+}
 
-fn cd(paths: &mut SplitWhitespace) {
+impl From<io::Error> for RshError {
+    fn from(e: io::Error) -> Self {
+        RshError::Io(e)
+    }
+}
+
+type BuiltinCommand = fn(&mut SplitWhitespace) -> Result<(), RshError>;
+
+fn cd(paths: &mut SplitWhitespace) -> Result<(), RshError> {
     if let Some(path) = paths.next() {
-        if let Err(error) = env::set_current_dir(path) {
-            println!("Cannot change directory: {}", error)
-        }
+        try!(env::set_current_dir(path));
     }
+    Ok(())
 }
 
-fn pwd(_: &mut SplitWhitespace) {
-    match env::current_dir() {
-        Ok(current_dir) => println!("{}", current_dir.display()),
-        Err(error) => println!("Cannot print current dir: {}", error),
-    }
+fn pwd(_: &mut SplitWhitespace) -> Result<(), RshError> {
+    let dir = try!(env::current_dir());
+    println!("{}", dir.display());
+    Ok(())
 }
 
-fn execute_command(line: String, builtins: &HashMap<&str, BuiltinCommand>) {
+fn execute_command(line: String, builtins: &HashMap<&str, BuiltinCommand>) -> Result<(), RshError> {
     let mut words = line.split_whitespace();
 
     if let Some(program) = words.next() {
         if builtins.contains_key(program) {
-            builtins[program](&mut words);
-            return;
+            try!(builtins[program](&mut words));
+        } else {
+            let mut command = Command::new(program);
+            for word in words {
+                command.arg(word);
+            }
+            try!(command.spawn());
         }
-
-        let mut command = Command::new(program);
-        for word in words {
-            command.arg(word);
-        }
-
-        command.spawn();
     }
+    Ok(())
 }
 
 fn main() {
@@ -48,6 +57,9 @@ fn main() {
     let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
-        execute_command(line.unwrap(), &builtins);
+        match execute_command(line.unwrap(), &builtins) {
+            Err(RshError::Io(e)) => println!("IO Error: {}", e),
+            _ => ()
+        }
     }
 }
